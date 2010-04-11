2e87d561e9551f13e50bc1c1b4c8c8a06eadc912
Adrian Sanchez <adrian.saz@gmail.com>
2010-04-11 17:38:23 -0500

    Special custom file with better font face

---
+++ custom-sample.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode t)
+ '(show-paren-mode t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#020202" :foreground "#C3C3C3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "bitstream" :family "Bitstream Vera Sans Mono"))))
+ '(ecb-default-highlight-face ((((class color) (background dark)) (:background "#883333")))))
