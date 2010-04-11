67241399af0828850b19f085578e1a052b654291
Adrian Sanchez <adrian.saz@gmail.com>
2010-04-11 15:33:32 -0500

    Refactor, add comments and linespaces

--- starter-kit-ror.el
+++ starter-kit-ror.el
@@ -1,13 +1,18 @@
 ;;; starter-kit-ror.el --- Ruby on Rails helpful tools
 
+
 (add-to-list 'load-path (concat dotfiles-dir "/vendor"))
 
+;;; Yaml mode
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/yaml"))
+
+;;; Rinari (Ruby on Rails)
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/rinari"))
 (require 'rinari)
 
 ;;; Haml mode
 (require 'haml-mode)
 (add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
+
 
 (provide 'starter-kit-ror)
