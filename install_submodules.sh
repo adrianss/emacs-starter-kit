e8446a4d32c0a3b100af0594a8fb2a514c7438e3
Adrian Sanchez <adrian.saz@gmail.com>
2010-04-11 14:52:57 -0500

    fix mispelled directory name From utils/jump to util/jump

--- install_submodules.sh
+++ install_submodules.sh
@@ -2,6 +2,6 @@
 git submodule init vendor/rinari
 git submodule update vendor/rinari
 cd vendor/rinari
-git submodule init utils/jump
-git submodule update utils/jump
+git submodule init util/jump
+git submodule update util/jump
 cd ../..
