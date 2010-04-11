771419441e594362d8cf5c16024fd76189e5b3da
Adrian Sanchez <adrian.saz@gmail.com>
2010-04-11 15:31:20 -0500

    Haml mode added

--- starter-kit-ror.el
+++ starter-kit-ror.el
@@ -1,7 +1,13 @@
 ;;; starter-kit-ror.el --- Ruby on Rails helpful tools
 
+(add-to-list 'load-path (concat dotfiles-dir "/vendor"))
+
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/yaml"))
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/rinari"))
 (require 'rinari)
+
+;;; Haml mode
+(require 'haml-mode)
+(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
 
 (provide 'starter-kit-ror)
