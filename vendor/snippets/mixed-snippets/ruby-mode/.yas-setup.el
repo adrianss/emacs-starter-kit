37b16d96d5914c25823bf8a9d5c67a11860f5de4
Adrian Sanchez <adrian.saz@gmail.com>
2010-05-03 15:17:14 -0500

    fix error void variable

--- vendor/snippets/mixed-snippets/ruby-mode/.yas-setup.el
+++ vendor/snippets/mixed-snippets/ruby-mode/.yas-setup.el
@@ -17,58 +17,58 @@
   (interactive "r\nsName your partial: ")
   (let* ((path (buffer-file-name)) ending)
     (if (string-match "view" path)
-	(let ((ending (and (string-match ".+?\\(\\.[^/]*\\)$" path)
-			   (match-string 1 path)))
-	      (partial-name
-	       (replace-regexp-in-string "[[:space:]]+" "_" partial-name)))
-	  (kill-region begin end)
-	  (if (string-match "\\(.+\\)/\\(.+\\)" partial-name)
-	      (let ((default-directory (expand-file-name (match-string 1 partial-name)
-							 (expand-file-name ".."))))
-		(find-file (concat "_" (match-string 2 partial-name) ending)))
-	    (find-file (concat "_" partial-name ending)))
-	  (yank) (pop-to-buffer nil)
-	  (insert (concat "<%= render :partial => '" partial-name "' %>\n")))
+        (let ((ending (and (string-match ".+?\\(\\.[^/]*\\)$" path)
+                           (match-string 1 path)))
+              (partial-name
+               (replace-regexp-in-string "[[:space:]]+" "_" partial-name)))
+          (kill-region begin end)
+          (if (string-match "\\(.+\\)/\\(.+\\)" partial-name)
+              (let ((default-directory (expand-file-name (match-string 1 partial-name)
+                                                         (expand-file-name ".."))))
+                (find-file (concat "_" (match-string 2 partial-name) ending)))
+            (find-file (concat "_" partial-name ending)))
+          (yank) (pop-to-buffer nil)
+          (insert (concat "<%= render :partial => '" partial-name "' %>\n")))
       (message "not in a view"))))
 ;;;
 ;;; The TextMate "intelligent" migration snippet
 ;;
 (defvar yas/rails-intelligent-migration-snippet-bits
-      '((:rename_column . ((:up   . "rename_column :${1:table_name}, :${2:column_name}, :${3:new_column_name}$0")
-                           (:down . "rename_column :$1, :$3, :$2" )))
+  '((:rename_column . ((:up   . "rename_column :${1:table_name}, :${2:column_name}, :${3:new_column_name}$0")
+                       (:down . "rename_column :$1, :$3, :$2" )))
 
-        (:rename_column_continue . ((:up   . "rename_column :${1:table_name}, :${2:column_name}, :${3:new_column_name}\nmncc$0")
-                                    (:down . "rename_column :$1, :$3, :$2" )))
+    (:rename_column_continue . ((:up   . "rename_column :${1:table_name}, :${2:column_name}, :${3:new_column_name}\nmncc$0")
+                                (:down . "rename_column :$1, :$3, :$2" )))
 
-        (:rename_table . ((:up   . "rename_table :${1:old_table_name}, :${2:new_table_name}$0")
-                          (:down . "rename_table :$2, :$1" )))
+    (:rename_table . ((:up   . "rename_table :${1:old_table_name}, :${2:new_table_name}$0")
+                      (:down . "rename_table :$2, :$1" )))
 
-        (:rename_table_continue . ((:up   . "rename_table :${1:old_table_name}, :${2:new_table_name}\nmntc$0")
-                                   (:down . "rename_table :$2, :$1" )))
+    (:rename_table_continue . ((:up   . "rename_table :${1:old_table_name}, :${2:new_table_name}\nmntc$0")
+                               (:down . "rename_table :$2, :$1" )))
 
-        (:add_remove_column . ((:up   . "add_column :${1:table_name}, :${2:column_name}, :${3:string}$0")
-                               (:down . "remove_column :$1, :$2" )))
-        
-        (:add_remove_column_continue . ((:up   . "add_column :${1:table_name}, :${2:column_name}, :${3:string}\nmarcc$0")
-                                        (:down . "remove_column :$1, :$2" )))
-        
-        (:remove_add_column . ((:up   . "remove_column :${1:table_name}, :${2:column_name}$0")
-                               (:down . "add_column :$1, :$2, :$3{string}" )))
+    (:add_remove_column . ((:up   . "add_column :${1:table_name}, :${2:column_name}, :${3:string}$0")
+                           (:down . "remove_column :$1, :$2" )))
 
-        (:create_drop_table . ((:up   . "create_table :${1:table_name}, :force . true do |t|\nt.$0\nt.timestamps\nend")
-                               (:down . "drop_table :$1" )))
+    (:add_remove_column_continue . ((:up   . "add_column :${1:table_name}, :${2:column_name}, :${3:string}\nmarcc$0")
+                                    (:down . "remove_column :$1, :$2" )))
 
-        (:change_change_table . ((:up   . "change_table :${1:table_name} do |t|\nt.$0\nend")
-                                 (:down . "change_table :$1 do |t|\nend" )))
+    (:remove_add_column . ((:up   . "remove_column :${1:table_name}, :${2:column_name}$0")
+                           (:down . "add_column :$1, :$2, :$3{string}" )))
 
-        (:add_remove_index . ((:up   . "add_index :${1:table_name}, :${2:column_name}$0")
-                              (:down . "remove_index :$1, :$2" )))
+    (:create_drop_table . ((:up   . "create_table :${1:table_name}, :force . true do |t|\nt.$0\nt.timestamps\nend")
+                           (:down . "drop_table :$1" )))
 
-        (:add_remove_unique_index . ((:up   . "add_index :${1:table_name}, ${2:[:${3:column_name}${4:, :${5:column_name}}]}, :unique . true$0")
-                                     (:down . "remove_index :$1, :column . $2" )))
+    (:change_change_table . ((:up   . "change_table :${1:table_name} do |t|\nt.$0\nend")
+                             (:down . "change_table :$1 do |t|\nend" )))
 
-        (:add_remove_named_index . ((:up   . "add_index :${1:table_name}, [:${2:column_name}${3:, :${4:column_name}}], :name . \"${5:index_name}\"${6:, :unique . true}$0")
-                                    (:down . "remove_index :$1, :name . :$5" )))))
+    (:add_remove_index . ((:up   . "add_index :${1:table_name}, :${2:column_name}$0")
+                          (:down . "remove_index :$1, :$2" )))
+
+    (:add_remove_unique_index . ((:up   . "add_index :${1:table_name}, ${2:[:${3:column_name}${4:, :${5:column_name}}]}, :unique . true$0")
+                                 (:down . "remove_index :$1, :column . $2" )))
+
+    (:add_remove_named_index . ((:up   . "add_index :${1:table_name}, [:${2:column_name}${3:, :${4:column_name}}], :name . \"${5:index_name}\"${6:, :unique . true}$0")
+                                (:down . "remove_index :$1, :name . :$5" )))))
 
 
 (defun yas/rails-intelligent-migration-snippet (type)
@@ -86,12 +86,12 @@
       (yas/expand-snippet snippet))))
 
 (yas/define-condition-cache
-  yas/rails-intelligent-migration-snippet-condition-p
-  "Non-nil if an \"intelligent\" migration snippet should be expanded"
-  (and (yas/rails-migration-p)
-       (not (yas/rails-in-create-table-p))
-       (not (yas/rails-in-change-table-p))
-       (yas/rails-in-ruby-block-like "self\.up")))
+ yas/rails-intelligent-migration-snippet-condition-p
+ "Non-nil if an \"intelligent\" migration snippet should be expanded"
+ (and (yas/rails-migration-p)
+      (not (yas/rails-in-create-table-p))
+      (not (yas/rails-in-change-table-p))
+      (yas/rails-in-ruby-block-like "self\.up")))
 
 (defun yas/rails-in-ruby-block-like (regexp)
   (save-excursion
@@ -124,7 +124,7 @@
 
 (yas/define-condition-cache
  yas/rails-controller-p
-"Non-nil if the current buffer is a rails controller." 
+ "Non-nil if the current buffer is a rails controller."
  (and (yas/rails-root)
       (string-match "app/controllers/$" default-directory)))
 
@@ -145,4 +145,4 @@
 are recognized"
   (setq yas/rails-root-cache nil)
   (when (yas/rails-root)
-    (set (make-local-variable 'yas/mode-symbol) 'rails-mode))) 
+    (set (make-local-variable 'yas/mode-symbol) 'rails-mode)))
